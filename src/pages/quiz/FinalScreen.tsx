5f706ff3f9dda2d20f4ceb8727f29be5b9863b85
ahmadsoni <ahmadshonhaji476@gmail.com>
2023-01-18 13:17:44 +0700

    update location quiz :package:

--- src/pages/quiz/FinalScreen.tsx
+++ src/pages/quiz/FinalScreen.tsx
@@ -29,7 +29,7 @@ export default function FinalScreen() {
 	const dispatch = useDispatch();
 	const handleBackToSettings = () => {
 		setReturnToSettings(true);
-		window.location.href = '/quiz';
+		window.location.href = '/';
 		dispatch(handleScoreChange(0));
 		dispatch(handleWrongAnswer(0));
 		dispatch(handleAmountChange(10));
